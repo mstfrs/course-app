d6410753325b05014bde2049bc899359f1152549
mstfrs <mstfrs@gmail.com>
2022-08-23 20:15:43 +0300

    modal updated

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,19 +1,21 @@
-import React, { useContext } from 'react'
-import './CardsStyle.css'
-import logo from '../../assets/globalai.png'
-import { useNavigate } from 'react-router-dom'
-import ModalContext from '../../context/ModalContext'
+import React, { useContext, useState } from "react";
+import "./CardsStyle.css";
+import logo from "../../assets/globalai.png";
+import ModalContext from "../../context/ModalContext";
+import Modal from "react-modal";
+import parse from "html-react-parser";
 
 export const Cards = ({ course, tabid }) => {
-  const { setIsModalOpen } = useContext(ModalContext)
-  let navigate = useNavigate()
-  const moreClick = () => {
-    navigate('/', { state: { course, tabid } })
-    setIsModalOpen(true)
-  }
+  const { setIsModalOpen, isModalOpen } = useContext(ModalContext);
+  const [currentCourse, setCurrentCourse] = useState("")
 
-  var tabstatement
-  if (tabid === 'mycourse-tab' || tabid === '') {
+  const moreClick = (e) => {
+    setIsModalOpen(true);  
+    setCurrentCourse(course)  
+  };
+
+  var tabstatement;
+  if (tabid === "mycourse-tab" || tabid === "") {
     tabstatement = (
       <div className="course-card">
         <h1 className="course-title">{course.title}</h1>
@@ -22,8 +24,8 @@ export const Cards = ({ course, tabid }) => {
           View More
         </button>
       </div>
-    )
-  } else if (tabid === 'allcourse-tab') {
+    );
+  } else if (tabid === "allcourse-tab") {
     tabstatement = (
       <div className="course-card">
         <h1 className="course-title">{course.title.rendered}</h1>
@@ -32,8 +34,51 @@ export const Cards = ({ course, tabid }) => {
           View More
         </button>
       </div>
-    )
+    );
   }
+  Modal.setAppElement('#root')
+  return (
+    <>
+      {tabstatement}
+      {currentCourse!==""?
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        className="Modal overlayAnimationIn: 'customEnterOverlayAnimation',
+          overlayAnimationOut: 'customLeaveOverlayAnimation',
+          modalAnimationIn: 'customEnterModalAnimation',
+          modalAnimationOut: 'customLeaveModalAnimation',"
+        overlayClassName="Overlay"
+        contentClassName="Content"
+      >
+        <i
+          className="fa fa-times fa-2x"
+          aria-hidden="true"
+          onClick={() => setIsModalOpen(false)}
+        ></i>
+        {tabid === "mycourse-tab" || tabid === "" ? (
+          <div className="details-container">
+            <img className="course-image" src={logo} alt="" />
+            <h1 className="title">{currentCourse.title.toUpperCase()}</h1>
+            <br />
+            <p className="description">{currentCourse.description}</p>
 
-  return <>{tabstatement}</>
-}
+            <a href={currentCourse.link}>
+              <h3 className="details-link">Course Details</h3>
+            </a>
+          </div>
+        ) : (
+          <div className="details-container">
+            <h1 className="course-title">{currentCourse.title.rendered}</h1>
+
+            {parse(currentCourse.content.rendered)}
+          </div>
+        )}
+
+        <button className="modal-btn" onClick={() => setIsModalOpen(false)}>
+          Close
+        </button>
+      </Modal>:null}
+    </>
+  );
+};
--- src/router/Approuter.js
+++ src/router/Approuter.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Courses from '../pages/courses/Courses'
+// import DetailsModal from "../components/Modal/DetailsModal"
 
 const Approuter = () => {
   return (
@@ -9,6 +10,7 @@ const Approuter = () => {
       <Routes>
         <Route>
           <Route path="/" element={<Courses />} />
+          {/* <Route path=":id" element={<DetailsModal/> } /> */}
         </Route>
       </Routes>
     </BrowserRouter>
